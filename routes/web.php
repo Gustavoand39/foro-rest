56f5935ab456d93bff90cad3199e096130a56d70
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2022-10-18 11:34:34 -0500

    18/10/22

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,25 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/topics', 'TopicController@index');
+/* $router->get('/topics', 'TopicController@index');
+$router->get('/topics/{id}', 'TopicController@show');
+$router->post('/topics', 'TopicController@store');
+$router->put('/topics/{id}', 'TopicController@update');
+$router->delete('/topics/{id}', 'TopicController@destroy');
+
+$router->get('/users', 'UserController@index');
+$router->get('/users/{id}', 'UserController@show');
+$router->post('/users', 'UserController@store');
+$router->put('/users/{id}', 'UserController@update');
+$router->delete('/users/{id}', 'UserController@destroy'); */
+
+function resource($router, $url, $model){
+    $router->get($url, $model.'Controller@index');
+    $router->get($url.'/{id}', $model.'Controller@show');
+    $router->post($url, '$model.TController@store');
+    $router->put($url.'/{id}', $model.'Controller@update');
+    $router->delete($url.'/{id}', $model.'Controller@destroy');
+}
+
+resource($router, '/topics', 'Topic');
+resource($router, '/users', 'User');
